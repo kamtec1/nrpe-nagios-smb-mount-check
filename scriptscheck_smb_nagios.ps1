db5b9ac64045a58d6fa67d6ee0bdf1ab317f070c
Sergey Babkevych <kamtec1@gmail.com>
2022-01-10 16:32:53 +0200

    Update scriptscheck_smb_nagios.ps1

--- scriptscheck_smb_nagios.ps1
+++ scriptscheck_smb_nagios.ps1
@@ -1,4 +1,13 @@
-# TEST SMB NAGIOS VIA MOUNT
+#nrpe-nagios-smb-mount-check v 1.0
+#Last update: 10.01.2022
+#Written by Sergey Babkevych (kamtec1) SecurityInet 
+#SecurityInet Web site for updates regarding new release
+#https://www.securityinet.com
+#Github link for updates:
+#https://github.com/kamtec1/nrpe-nagios-smb-mount-check/
+#nrpe-nagios-smb-mount-check is licensed under the GNU General Public License v3.0
+#If you hgave questions you may send me a massage : kamtec1@gmail.com or sergey@securityinet.com
+
 
 
 Set-Variable OK 0 -option Constant
